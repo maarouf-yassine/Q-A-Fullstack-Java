aea04c3fe2648b79949aa6066d77f5f7e42c1152
maarouf yassine <>
2021-03-10 06:24:26 +0200

    Updated CORS to Allow API calls from different URLs

--- src/main/java/com/maarouf/qaApp/controller/QuestionController.java
+++ src/main/java/com/maarouf/qaApp/controller/QuestionController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class QuestionController {
     private final QuestionsService questionsService;
 
